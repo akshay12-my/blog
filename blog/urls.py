ab546f4dd6412c46c45cfe794ea343d7bc92c287
akshay12-my <akshaymuktikar69@gmail.com>
2020-10-24 11:23:34 +0530

    first

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from    django.urls     import  path
+from django.conf.urls import url
+from .  import views                                
+
+app_name        =       'blog'     
+urlpatterns     =       [                                                                               
+        #post   view                                
+        path('',views.post_list,name='post_list'),
+        path('',views.PostListView.as_view(),name='post_list'),                             
+        path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail,name='post_detail'),
+        path('<int:post_id>/share/',views.post_share,name='post_share'),  
+]
+
